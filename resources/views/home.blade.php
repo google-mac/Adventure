68a67882faddf680846db6d49945785fe704dae9
Daylon Crider <daylon.crider@gmail.com>
2018-10-06 15:30:18 -0700

    Added basic home page to test functionality

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Adventure - Home</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <p> Welcome home!</p>
+            </div>
+        </div>
+    </body>
+</html>
